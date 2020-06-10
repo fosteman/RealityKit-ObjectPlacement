c5bb5972f9b5cf7e8d0892930b212f8e180a9197
Timothy Fosteman <dgrz5.0@gmail.com>
2020-06-10 13:47:02 -0400

    Model Picker View

--- Tutorial/ContentView.swift
+++ Tutorial/ContentView.swift
@@ -10,8 +10,14 @@ import SwiftUI
 import RealityKit
 
 struct ContentView : View {
+    
+    var models: [String] = ["toy_biplane", "tv_retro", "toy_drummer"]
+    
     var body: some View {
-        Text("Hey there!")
+        ZStack(alignment: .bottom) {
+            ARViewContainer()
+            ModelPickerView(models: self.models)
+        }
     }
 }
 
@@ -31,6 +37,35 @@ struct ARViewContainer: UIViewRepresentable {
     
 }
 
+struct ModelPickerView: View {
+    var models: [String]
+    
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 30) {
+                //
+                ForEach(0 ..< self.models.count) {
+                    index in
+                    Button(action: {
+                        print("Debug selected model: \(self.models[index])" )
+                    }, label: {
+                        Image(uiImage: UIImage(named: self.models[index])!)
+                        .resizable()
+                            .frame(height: 80)
+                            .aspectRatio(1/1, contentMode: .fit)
+                    })
+                    .buttonStyle(PlainButtonStyle())
+                        .background(Color.white)
+                    .cornerRadius(12)
+                    
+                }
+            }
+        }
+        .padding(20)
+        .background(Color.black.opacity(0.5))
+    }
+}
+
 #if DEBUG
 struct ContentView_Previews : PreviewProvider {
     static var previews: some View {
@@ -38,3 +73,5 @@ struct ContentView_Previews : PreviewProvider {
     }
 }
 #endif
+
+
